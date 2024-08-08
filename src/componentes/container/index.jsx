12a842317101879f079432ad01a31e763db7ab45
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-08-08 16:25:41 -0300

    fonte e componentes

---
+++ src/componentes/container/index.jsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+const Container = () => {
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.titulo}>Site das Provas</div>
+            </div>
+        </>
+    )
+}
+
+export default Container;
